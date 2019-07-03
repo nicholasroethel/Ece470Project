b3c6e45083fff3b3a2039cf4354e6b51a81a4b5d
Nicholas Roethel <nicholasroethel@uvic.ca>
2019-07-03 14:23:15 -0700

    adding broken random number generator

--- GADecrypt.py
+++ GADecrypt.py
@@ -1,5 +1,17 @@
+import nltk
+import random
+from nltk.corpus import words
+from random import sample
+
+def genRandom(n):
+	randString = ' '.join(sample(words.words(), n))
+	print(randString)
+
+
 def main():
   print("Hello World!")
+  n = 5
+  genRandom(n)
   
 if __name__== "__main__":
   main()
