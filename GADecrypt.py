7f81d7aad1c76da757d3f19d2579f253a7d579f0
Nicholas Roethel <nicholasroethel@uvic.ca>
2019-07-03 15:41:04 -0700

    added function to generate a string of length n, comprised of random words

--- GADecrypt.py
+++ GADecrypt.py
@@ -4,13 +4,33 @@ from nltk.corpus import words
 from random import sample
 
 def genRandom(n):
-	randString = ' '.join(sample(words.words(), n))
-	print(randString)
+	wordlist = words.words()
+	randString = ''
+
+
+	while len(randString) < n: #keeps adding to randstring until its length n
+		#print(len(randString))
+
+		if ((len(randString)<1)  or (len(randString) > n-3)): #gets first part or resets to speed up probability of finding proper string length
+			randString =  ''
+			while(len(randString)<1):
+				tmpword = random.choice(wordlist)
+				if len(tmpword) < n:
+					randString = tmpword
+
+		tmpword = random.choice(wordlist)
+		tmplen = len(tmpword)
+
+		if (len(randString) + 1 + tmplen)<=n: #add to the string if possible
+			randString = randString + ' ' + tmpword 
+
+	return randString
 
 def main():
   nltk.download('words')
-  n = 5
-  genRandom(n)
+  n = 50
+  randString = genRandom(n)
+  print(randString)
   
 if __name__== "__main__":
   main()
